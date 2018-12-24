d1f4c2950a8ac051fdffca4ef72b4e228220bfef
Aakash Pandey <Aakash.pandey@live.com>
2018-12-24 18:19:22 +0530

    Update script.js

--- CacheAndUpgrade/script.js
+++ CacheAndUpgrade/script.js
@@ -1,20 +1,25 @@
-
+    // Reload page on sw change
     navigator.serviceWorker.addEventListener('controllerchange', () => {
         window.location.reload()
     });
+
     var newSW;
     // Register service worker
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/sw.js')
             .then((reg) => {
+                // Check if an installed sw is waiting
+                if(newSW = reg.waiting) {
+                    showBtn();
+                }
                 reg.addEventListener('updatefound', () => {
                     // Copy reference of new worker being installed
                     newSW = reg.installing;
                     newSW.addEventListener('statechange', () => {
-                        // On service worker state changed(except initial one)
+                        // service worker state changed except initial one
                         if (newSW.state === 'installed' && navigator.serviceWorker.controller) {
                             // display upgrade button
-                            document.getElementById('up').style.display = "block";
+                            showBtn();
                         }
                     })
                 })
@@ -28,5 +33,9 @@
         // Send message to new service worker
         newSW.postMessage({ action: 'clearOld' });
         newSW.postMessage({ action: 'skipWaiting' });
+    }
+
+    var showBtn = () => {
+        document.getElementById('up').style.display = "block";
     }
 
